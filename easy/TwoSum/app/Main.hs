c459496f83bac03a0c4757ef9e5704aa1c7c50c4
Daniel Rank <dwrank@gmail.com>
2023-01-31 22:47:00 -0800

    TwoSum: Add implementation

--- easy/TwoSum/app/Main.hs
+++ easy/TwoSum/app/Main.hs
@@ -1,6 +1,24 @@
 module Main (main) where
 
-import Lib
+import qualified Data.Map as Map
 
 main :: IO ()
-main = someFunc
+main = do
+    print $ twoSum [2,7,11,15] 9
+    print $ twoSum [2,7,11,15] 22
+    print $ twoSum [2,7,11,15] 26
+    print $ twoSum [2,7,11,15] 14
+    print $ twoSum [2,7,11,15] 99
+
+twoSum :: [Int] -> Int -> Maybe (Int, Int)
+twoSum nums target =
+    let numsWithIndices = zip nums [0..]
+    in twoSum' numsWithIndices Map.empty target
+
+twoSum' :: [(Int, Int)] -> Map.Map Int Int -> Int -> Maybe (Int, Int)
+twoSum' [] _ _ = Nothing
+twoSum' ((num, i):rest) seen target =
+    let diff = target - num
+    in case Map.lookup diff seen of
+         Nothing -> twoSum' rest (Map.insert num i seen) target
+         Just j -> Just (j, i)
